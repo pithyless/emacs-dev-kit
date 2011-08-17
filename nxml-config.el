28936a195adb56e2e6ecaaa4aa2311eaf9c06979
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-08-17 16:26:24 +0200

    changes some nxml-mode defaults

--- nxml-config.el
+++ nxml-config.el
@@ -12,10 +12,16 @@
 ;; pom files should be treated as xml files
 (add-to-list 'auto-mode-alist '("\\.pom$"   . nxml-mode))
 
-(setq nxml-child-indent 4)
+(setq nxml-child-indent 2)
 (setq nxml-attribute-indent 4)
 (setq nxml-auto-insert-xml-declaration-flag nil)
 (setq nxml-bind-meta-tab-to-complete-flag t)
 (setq nxml-slash-auto-complete-flag t)
+(setq rng-nxml-auto-validate-flag nil)
+
+(eval-after-load 'nxml-mode
+  '(add-hook 'nxml-mode-hook
+             (lambda ()
+               (local-set-key (kbd "M-RET") 'nxml-finish-element))))
 
 (provide 'nxml-config)
